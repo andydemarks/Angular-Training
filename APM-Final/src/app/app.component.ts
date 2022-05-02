8ef669068183afd4ec760011d581b58c604745ed
Andrew DeMarco <97972359+andydemarks@users.noreply.github.com>
2022-05-02 19:38:30 -0400

    updated the comment on some of the buggy code i didn't fix from the training video

--- APM-Final/src/app/app.component.ts
+++ APM-Final/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
 //updated the buttons to have no text underline
+//!! This code is buggy and I do not like the way this routing was done by the training
+//!! would have done it differently by actually using angular routing.
 
 @Component({
   selector: 'pm-root',
   template: `
     <nav class='navbar navbar-expand navbar-light bg-light'>
-        <a class='navbar-brand' style="text-decoration: none">{{pageTitle}}</a>
+        <a class='navbar-brand' style='text-decoration: none'>Nasa Mars Rover List</a>
         <ul class='nav nav-pills'>
-          <li><a class='nav-link' routerLinkActive='active' style="text-decoration: none" routerLink='/welcome'>Home</a></li>
-          <li><a class='nav-link' routerLinkActive='active' style="text-decoration: none" routerLink='/products'>Product List</a></li>
+          <li><a class='nav-link' routerLinkActive='active' style='text-decoration: none' routerLink='/welcome'>Home</a></li>
+          <li><a class='nav-link' routerLinkActive='active' style='text-decoration: none' routerLink='/products'>Product List</a></li>
         </ul>
     </nav>
     <div class='container'>
